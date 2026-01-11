3753e12f912806ac5ba8eb6216fed7d70286aa98
Louis Antonio D. Lapayag <louislapayag16@gmail.com>
2026-01-11 20:00:21 +0800

    Fix: notify CartItem property changes so UI updates quantities; center product images with ImageBrush ellipses

--- CoffeeShopKiosk/Models/CartItem.cs
+++ CoffeeShopKiosk/Models/CartItem.cs
@@ -1,12 +1,34 @@
 using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace CoffeeShopKiosk.Models
 {
-    public class CartItem
+    public class CartItem : INotifyPropertyChanged
     {
+        private int _quantity = 1;
+
         public ProductModel Product { get; set; }
-        public int Quantity { get; set; } = 1;
+
+        public int Quantity
+        {
+            get => _quantity;
+            set
+            {
+                if (_quantity == value) return;
+                _quantity = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(LineTotal));
+            }
+        }
 
         public decimal LineTotal => (Product?.Price ?? 0m) * Quantity;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
